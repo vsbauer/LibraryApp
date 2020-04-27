ef61ebd40a4556fe8983330a390704a5a0ec456a
vsbauer <vlad.bauer24@gmail.com>
2020-04-27 20:18:12 +0300

    Initial commit

---
+++ app/src/main/java/com/github/vsbauer/libraryapp/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.github.vsbauer.libraryapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.github.vsbauer.libraryapp.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
