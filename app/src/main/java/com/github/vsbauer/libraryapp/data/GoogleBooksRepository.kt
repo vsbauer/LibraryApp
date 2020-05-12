98cf3961275bb526660d7cc495437e83156dca4c
vsbauer <vlad.bauer24@gmail.com>
2020-05-12 15:02:36 +0300

    refactor repository

--- app/src/main/java/com/github/vsbauer/libraryapp/data/GoogleBooksRepository.kt
+++ app/src/main/java/com/github/vsbauer/libraryapp/data/GoogleBooksRepository.kt
@@ -1,15 +1,13 @@
 package com.github.vsbauer.libraryapp.data
 
 import android.util.Log
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.liveData
 import com.github.vsbauer.libraryapp.data.models.Book
 
 object GoogleBooksRepository {
 
-    fun getBooks(search: String): LiveData<List<Book>> = liveData {
+    suspend fun getBooks(search: String): List<Book> {
         try {
-            emit(BookApiService.newInstance().getBooks(search).bookDTOS.map {
+            return BookApiService.newInstance().getBooks(search).bookDTOS.map {
                 Log.d("!!!Repos", it.toString())
                 val author = it.bookInfo.authors?.firstOrNull() ?: ""
                 Book(
@@ -19,9 +17,10 @@ object GoogleBooksRepository {
                     it.bookInfo.infoLink
                 )
 
-            })
+            }
         } catch (e: Exception) {
             Log.e("!!!MainRepository", e.toString())
+            return listOf()
         }
     }
 
